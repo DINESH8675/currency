67a7757b1939b593c7986b4b549d3b081071dc08
DINESH8675 <136824369+DINESH8675@users.noreply.github.com>
2023-08-17 18:58:21 +0530

    Create simple.py

---
+++ simple.py
@@ -0,0 +1,28 @@
+a=int(input("Enter value  for a ")) 
+b=a//500 #500
+c=a%500 
+d=c//200 #200
+e=c%200
+f=e//100 #100
+g=e%100
+h=g//50 #50
+i=g%50
+j=i//20 #20
+k=i%20
+l=k//10 #10
+m=k%10
+n=m//5 #5
+o=m%5
+p=o//2 #2
+q=o%2
+r=q//1 #1
+print(b)#500
+print(d)#200
+print(f)#100
+print(h)#50
+print(j)#20
+print(l)#10
+print(n)#5
+print(p)#2
+print(r)#1
+print(b+d+f+h+j+l+n+p+r)#count 
